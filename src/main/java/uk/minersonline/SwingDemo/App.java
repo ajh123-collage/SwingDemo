f86bc1890c6b63ded18face468654678e261954e
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-03-08 13:20:48 +0000

    feat: begin swing demo

---
+++ src/main/java/uk/minersonline/SwingDemo/App.java
@@ -0,0 +1,9 @@
+package uk.minersonline.SwingDemo;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Swing Demo");
+    }
+}
---
+++ src/main/java/uk/minersonline/SwingDemo/Board.java
@@ -0,0 +1,11 @@
+package uk.minersonline.SwingDemo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    public Board() {
+        setPreferredSize(new Dimension(640, 480));
+        setBackground(Color.CYAN);
+    }
+}
